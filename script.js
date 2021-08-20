60f3c98331e116a05016ff53fdc92f5df5aadfae
sinaya naik <sinayanaik@gmail.com>
2021-08-20 19:24:24 +0530

    added second

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 window.addEventListener('load', calculateTime)
-
+    //"Fri Aug 20 2021 19:09:38 GMT+0530 (India Standard Time)"
 function calculateTime() {
     let date = new Date();
     let dayNumber = date.getDay();
     let hour = date.getHours();
     let minute = date.getMinutes();
+    let second = date.getSeconds();
     let ampm = hour >= 12 ? 'PM' : 'AM';
     let dayNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    hour = hour % 12;
-    hour = hour ? 12 : '12';
+
+    if (hour == 12) {
+        hour = '12';
+    } else {
+        hour = hour % 12;
+    }
+
     hour = hour < 10 ? '0' + hour : hour;
     minute = minute < 10 ? '0' + minute : minute;
-
+    second = second < 10 ? '0' + second : second;
     document.getElementById('day').innerHTML = dayNames[dayNumber];
     document.getElementById('hour').innerHTML = hour;
     document.getElementById('minute').innerHTML = minute;
     document.getElementById('ampm').innerHTML = ampm;
+    document.getElementById('second').innerHTML = second;
     setTimeout(calculateTime, 200);
 }
